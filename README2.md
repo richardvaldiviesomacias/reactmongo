296e8c457c8f6884d9714a2716d1c3fc3cd824c9
Richard Valdivieso <rvaldivieso@masonite.com>
2017-08-17 20:57:09 -0400

    Adding mongo, react and redux

---
+++ README2.md
@@ -0,0 +1,17 @@
+## Mongodb - Redux - React
+
+### How to run this little app
+
+- Run npm install
+- Run the following command
+
+    ```
+    json-server --watch db.json
+    ```
+- Finally
+
+    ```
+    npm start
+    ```
+    
+ After the above steps you should be able to see the dev tools and check the output.
--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import minimongo from 'minimongo';
+import axios from 'axios';
+let LocalDb = minimongo.MemoryDb;
+let db = new LocalDb();
 
 class App extends Component {
+
+    initializeDB(response) {
+         response.data.forEach((contract)=>{
+            db.contracts.upsert(contract, function() {
+                console.log("save", contract);
+            });
+        })
+    }
+
+    componentDidMount() {
+        let that = this;
+        db.addCollection("contracts");
+
+        axios.get('http://localhost:3001/contacts')
+            .then(function (response) {
+                console.log(response)
+                that.initializeDB(response);
+                db.contracts.findOne({ id:1 }, {}, function(res) {
+                    console.log("Contract's name is: " + res.name);
+                });
+
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
 
 
     render() {
